8c3d7ebff419da2b1b8c3716138f59d40e9a0c35
Md. Hossni Mubarak <mhmrajib@gmail.com>
2021-02-05 02:12:41 +0600

    'pre_get_posts' hook removed

--- wp-display-faq.php
+++ wp-display-faq.php
@@ -3,11 +3,11 @@
  * Plugin Name: 	Display FAQs
  * Plugin URI:		http://wordpress.org/plugins/wp-display-faq/
  * Description: 	This Plugin will display FAQs in your page by using the shortcode: [wp_display_faq]
- * Version: 		1.1
+ * Version: 		  1.1
  * Author:		    HM Plugin
- * Author URI:	    https://hmplugin.com
- * License:         GPL-2.0+
- * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
+ * Author URI:	  https://hmplugin.com
+ * License:       GPL-2.0+
+ * License URI:   http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
 if ( ! defined('ABSPATH') ) exit;
@@ -23,14 +23,5 @@ define('WFP_VERSION', '1.1');
 require_once WFP_PATH . 'inc/' . WFP_CLS_PRFX . 'master.php';
 $wfp = new WFP_Master();
 $wfp->wfp_run();
-
-/* Sort team members like page order i.e. the number assigned */
-function team_custom_post_order_sort( $query ){
-    if ( $query->is_main_query() && is_post_type_archive( 'team' )){
-      $query->set( 'orderby', 'menu_order' );
-      $query->set( 'order' , 'DESC' );
-    }
-  }
-  add_action( 'pre_get_posts' , 'team_custom_post_order_sort' );
 
 register_deactivation_hook( __FILE__, array( $wfp, WFP_PRFX . 'unregister_settings' ) );
